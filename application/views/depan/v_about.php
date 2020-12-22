b3994dab3e59ea1958e5f38d8e75d9a7da74a6f3
fs-15 <fs-15@users.noreply.github.com>
2020-12-22 15:50:07 +0700

    project pertama

---
+++ application/views/depan/v_about.php
@@ -0,0 +1,274 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>About</title>
+    <link rel="shorcut icon" href="<?php echo base_url().'theme/images/icon.png'?>">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/bootstrap.min.css'?>">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,700" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/font-awesome.min.css'?>">
+    <!-- Simple Line Font -->
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/simple-line-icons.css'?>">
+    <!-- Owl Carousel -->
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/slick.css'?>">
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/slick-theme.css'?>">
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/owl.carousel.min.css'?>">
+    <!-- Main CSS -->
+    <link href="<?php echo base_url().'theme/css/style.css'?>" rel="stylesheet">
+</head>
+
+<body>
+    <!--============================= HEADER =============================-->
+    < <div class="header-topbar">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-6 col-sm-8 col-md-9">
+                    <div class="header-top_address">
+                        <div class="header-top_list">
+                            <span class="icon-phone"></span>085xxxxxxxxx
+                        </div>
+                        <div class="header-top_list">
+                            <span class="icon-envelope-open"></span>khabibalfatach@gmail.com
+                        </div>
+                        <div class="header-top_list">
+                            <span class="icon-location-pin"></span>Kebun Jeruk, Jakarta Barat
+                        </div>
+                    </div>
+                    <div class="header-top_login2">
+                        <a href="<?php echo site_url('contact');?>">Hubungi Kami</a>
+                    </div>
+                </div>
+                <div class="col-xs-6 col-sm-4 col-md-3">
+                    <div class="header-top_login mr-sm-3">
+                        <a href="<?php echo site_url('contact');?>">Hubungi Kami</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div data-toggle="affix" style="border-bottom:solid 1px #f2f2f2;">
+        <div class="container nav-menu2">
+            <div class="row">
+                <div class="col-md-12">
+                    <nav class="navbar navbar2 navbar-toggleable-md navbar-light bg-faded">
+                        <button class="navbar-toggler navbar-toggler2 navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown">
+                            <span class="icon-menu"></span>
+                        </button>
+                        <a href="<?php echo site_url('');?>" class="navbar-brand nav-brand2"><img class="img img-responsive" width="50px;" src="<?php echo base_url().'theme/images/logo.jpg' ?>"></a>
+                        <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('');?>">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('about');?>">About</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('guru');?>">Program</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('siswa');?>">Kemitraan</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('blog');?>">Berita</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('agenda');?>">Agenda</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('download');?>">Download</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('galeri');?>">Gallery</a>
+                                </li>
+                                <li class="nav-item">
+                                  <a class="nav-link" href="<?php echo site_url('contact');?>">Contact</a>
+                              </li>
+                              <li class="nav-item">
+                                <a class="nav-link" href="<?php echo site_url('bayar_zakat');?>">Bayar Zakat</a>
+                            </li>
+
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+</div>
+<section>
+</section>
+<!--//END HEADER -->
+<!--//END ABOUT IMAGE -->
+<!--============================= WELCOME TITLE =============================-->
+<section class="welcome_about">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7">
+                <h2>Kata Sambutan</h2>
+                <p>Puji syukur kami panjatkan ke hadirat Tuhan Yang Maha Esa atas karunia dan hidayah-Nya,
+                  Shalawat dan salam senantiasa tercurah Limpahkan kepada baginda Nabi Muhammad SAW yang telah membawa umatnya dari zaman kegelapan menuju zaman yang terang benderang dan yang kita harapkan syafaatnya nanti di yaumil qiyamah. Amiin..
+              </p>
+              <p>Pesantren Ekonomi Darul Uchwah didirikan atas dasar agama Islam yang berhaluan Ahlus sunnah wal jama’ah yang berperan aktif dalam membangun peradaban bangsa dan senantiasa berupaya terus menciptakan dan meningkatkan kualitas sumber daya manusia yang berfikir Kritis, Kreatif, dan Inovatif.
+                <p>Pesantren Ekonomi Darul Uchwah adalah salah satu lembaga pendidikan Islam yang dikembangkan dalam rangka menghadapi tuntunan zaman yang semakin maju dan menekan akan dibutuhkannya pendidikan yang berbasis ekonomi sehingga terwujudnya generasi muslim yang mampu bersaing dalam menghadapi pembangunan ekonomi dan dinamika bisnis.
+                </p>
+            </p>
+        </div>
+        <div class="col-md-5">
+            <img src="<?php echo base_url().'theme/images/ULANG1.jpg'?>" class="img-fluid" alt="#">
+        </div>
+    </div>
+</div>
+</section>
+<!--//END WELCOME TITLE -->
+<!--============================= TESTIMONIAL =============================-->
+<section class="testimonial">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2>Testimonial</h2>
+            </div>
+            <div class="col-md-12">
+                <div class="single-item">
+                    <div class="quote">
+                        <i class="fa fa-quote-left" aria-hidden="true"></i>
+                        <p class="quote_text">Santri tidak hanya bisa mengaji.</p>
+                        <div class="testi-img_block">
+                            <img src="<?php echo base_url().'theme/images/coba.jpg'?>" class="img-fluid" alt="#">
+                            <p><span>Squad Jmbl</span>Santri Pesantren Ekonomi Darul Uchwah </p>
+                        </div>
+                    </div>
+                    <div class="quote">
+                        <i class="fa fa-quote-left" aria-hidden="true"></i>
+                        <p class="quote_text">Santri tidak hanya bisa mengaji.</p>
+                        <div class="testi-img_block">
+                            <img src="<?php echo base_url().'theme/images/coba.jpg'?>" class="img-fluid" alt="#">
+                            <p><span>Rizky Maulana</span>Santri Pesantren Ekonomi Darul Uchwah </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--//END TESTIMONIAL -->
+<!--============================= DETAILED CHART =============================-->
+<div class="detailed_chart">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-6 col-md-3 chart_bottom">
+                <div class="chart-img">
+                    <img src="<?php echo base_url().'theme/images/chart-icon_1.png'?>" class="img-fluid" alt="chart_icon">
+                </div>
+                <div class="chart-text">
+                    <p><span class="counter"><?php echo $tot_guru;?></span> Ustadz
+                    </p>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-3 chart_bottom chart_top">
+                <div class="chart-img">
+                    <img src="<?php echo base_url().'theme/images/chart-icon_2.png'?>" class="img-fluid" alt="chart_icon">
+                </div>
+                <div class="chart-text">
+                    <p><span class="counter"><?php echo $tot_siswa;?></span> Santri 
+                    </p>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-3 chart_top">
+                <div class="chart-img">
+                    <img src="<?php echo base_url().'theme/images/chart-icon_3.png'?>" class="img-fluid" alt="chart_icon">
+                </div>
+                <div class="chart-text">
+                    <p><span class="counter"><?php echo $tot_files;?></span> Download
+                    </p>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-3">
+                <div class="chart-img">
+                    <img src="<?php echo base_url().'theme/images/chart-icon_4.png'?>" class="img-fluid" alt="chart_icon">
+                </div>
+                <div class="chart-text">
+                    <p><span class="counter"><?php echo $tot_agenda;?></span> Agenda</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--//END DETAILED CHART -->
+
+<!--============================= FOOTER =============================-->
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="foot-logo">
+                    <a href="<?php echo site_url();?>">
+                        <img src="<?php echo base_url().'theme/images/logo.jpg'?>" class="img-fluid" alt="footer_logo">
+                    </a>
+                    <p><?php echo date('Y');?> © copyright by people. <br>All rights reserved.</p>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="sitemap">
+                    <h3>Menu Utama</h3>
+                    <ul>
+                        <li><a href="<?php echo site_url();?>">Home</a></li>
+                        <li><a href="<?php echo site_url('about');?>">About</a></li>
+                        <li><a href="<?php echo site_url('artikel');?>">Blog </a></li>
+                        <li><a href="<?php echo site_url('galeri');?>">Gallery</a></li>
+                        <li><a href="<?php echo site_url('contact');?>">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-3">
+              <div class="sitemap">
+                  <h3>Akademik</h3>
+                  <ul>
+                    <li><a href="<?php echo site_url('guru');?>">Ustadz</a></li>
+                    <li><a href="<?php echo site_url('siswa');?>">Santri </a></li>
+                    <li><a href="<?php echo site_url('pengumuman');?>">Pengumuman</a></li>
+                    <li><a href="<?php echo site_url('agenda');?>">Agenda</a></li>
+                    <li><a href="<?php echo site_url('download');?>">Download</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="address">
+                <h3>Hubungi Kami</h3>
+                <p><span>Alamat: </span> Jakarta Barat</p>
+                <p>Email : khabibfebby@gmail.com
+                    <br> Phone : 085xxxxxxxxx</p>
+                    <ul class="footer-social-icons">
+                        <li><a href="#"><i class="fa fa-facebook fa-fb" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fa fa-linkedin fa-in" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter fa-tw" aria-hidden="true"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!--//END FOOTER -->
+<!-- jQuery, Bootstrap JS. -->
+<script src="<?php echo base_url().'theme/js/jquery.min.js'?>"></script>
+<script src="<?php echo base_url().'theme/js/tether.min.js'?>"></script>
+<script src="<?php echo base_url().'theme/js/bootstrap.min.js'?>"></script>
+<!-- Plugins -->
+<script src="<?php echo base_url().'theme/js/slick.min.js'?>"></script>
+<script src="<?php echo base_url().'theme/js/waypoints.min.js'?>"></script>
+<script src="<?php echo base_url().'theme/js/counterup.min.js'?>"></script>
+<script src="<?php echo base_url().'theme/js/owl.carousel.min.js'?>"></script>
+<script src="<?php echo base_url().'theme/js/validate.js'?>"></script>
+<script src="<?php echo base_url().'theme/js/tweetie.min.js'?>"></script>
+<!-- Subscribe -->
+<script src="<?php echo base_url().'theme/js/subscribe.js'?>"></script>
+<!-- Script JS -->
+<script src="<?php echo base_url().'theme/js/script.js'?>"></script>
+</body>
+
+</html>
